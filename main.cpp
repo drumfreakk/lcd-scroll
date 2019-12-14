e91ec23badc3e3dfa47dc78e2afe68c44585662a
Kieran H <kieran.houtgraaf@gmail.com>
2019-12-14 10:41:35 +0100

    Added spaces, remove error

--- main.cpp
+++ main.cpp
@@ -3,62 +3,95 @@
 #include <string.h>
 
 #define DISPLAY_LEN 16
+#define SPLIT_LEN 2
+
+void dump(const char *data_buffer, const unsigned int length) {
+    unsigned char byte;
+    unsigned int i, j;
+    for(i=0; i < length; i++) {
+        byte = data_buffer[i];
+        printf("%02x ", data_buffer[i]);  // display byte in hex
+        if(((i%17)==16) || (i==length-1)) {
+            for(j=0; j < 16-(i%17); j++)
+                printf("   ");
+            printf("| ");
+            for(j=(i-(i%17)); j <= i; j++) {  // display printable bytes from line
+                byte = data_buffer[j];
+                if((byte > 31) && (byte < 127)) // outside printable char range
+                    printf("%c", byte);
+                else
+                    printf(".");
+            }
+            printf("\n"); // end of the dump line (each line 16 bytes)
+        } // end if
+    } // end for
+    printf("\n");
+}
 
 int getLen(char* str);
 
 int main() {
 
-    char* toDisplay = "Hello my name is Kieran";
-    int len = getLen(toDisplay);
+    char* input = "Hello my name is Kieran";
+
+
+    int len = getLen(input);
+
+    char* toDisplay = new char[len + SPLIT_LEN];
+
+    for(int i = 0; i < len+SPLIT_LEN-1; i++){
+        if(i < len-1) {
+            toDisplay[i] = input[i];
+        } else {
+            toDisplay[i] = ' ';
+        }
+    }
+    toDisplay[len+SPLIT_LEN-1] = '\0';
+
+    len = getLen(toDisplay);
+    dump(toDisplay, len+5);
 
     std::cout << len << std::endl;
 
     char text[DISPLAY_LEN];
     int step = 0;
-
     char* first = NULL;
+    bool done = false;
+    int count = 0;
+    int countb;
 
-    while(step < 24){
-//        for(int i = 0; i < DISPLAY_LEN + 1; i++){
-//            if(i+step < len){
-//                text[i] = toDisplay[i + step];
-//                std::cout << "a";
-//            }else if(i+step > len+2){
-//                text[i] = 'x';//toDisplay[1];
-//                std::cout << "b";
-//            }/*else if(i+step >= len){
-//                text[i] = ' ';
-//                std::cout << "c";
-//            }*/
-//        }
-//        text[DISPLAY_LEN + 1] = '\0';
-first = &toDisplay[step];
-
-        bool done = false;
-        int count = 0;
+    while(step < len){
+        first = &toDisplay[step];
+        done = false;
+        count = 0;
         while(!done){
             if(first[count] == '\0') {
                 done = true;
+//                text[count] = '\0';
                 continue;
             }
             text[count] = first[count];
             count++;
         }
         done = false;
-        int countb = count;
+        countb = count;
         while(!done){
-            if(countb >= 15){
+            if(countb >= DISPLAY_LEN-1){
                 done = true;
             }
             text[countb] = toDisplay[countb - count];
             countb++;
         }
+        text[16] = '\0';
 
         std::cout << "[" << step << "]:\t" << text << std::endl;
+//        dump(text, 17);
         step++;
     }
     std::cout << "end" << std::endl;
 
+
+    delete[] toDisplay;
     return 0;
 }
 
